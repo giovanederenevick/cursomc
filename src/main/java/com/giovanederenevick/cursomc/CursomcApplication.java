936cad5057ebd348722cc8e9ae52299ce380358c
Giovane Derenevick <giovanedfilho@gmail.com>
2020-02-21 21:23:40 -0300

    Criando operacao de instanciacao

--- src/main/java/com/giovanederenevick/cursomc/CursomcApplication.java
+++ src/main/java/com/giovanederenevick/cursomc/CursomcApplication.java
@@ -1,13 +1,36 @@
 package com.giovanederenevick.cursomc;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.giovanederenevick.cursomc.domain.Categoria;
+import com.giovanederenevick.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+				
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		ArrayList<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		categoriaRepository.saveAll(lista);
 	}
 
 }
