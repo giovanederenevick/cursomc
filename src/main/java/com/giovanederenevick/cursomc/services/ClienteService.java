460ff81542ac97aee85f5ce33974cf9e0480e710
Giovane Derenevick <giovanedfilho@gmail.com>
2020-02-23 23:23:09 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/giovanederenevick/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.giovanederenevick.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.giovanederenevick.cursomc.domain.Cliente;
+import com.giovanederenevick.cursomc.repositories.ClienteRepository;
+import com.giovanederenevick.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository categoriaRepository;
+
+	public Cliente buscar(Integer id){
+		
+		Optional<Cliente> obj = categoriaRepository.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
