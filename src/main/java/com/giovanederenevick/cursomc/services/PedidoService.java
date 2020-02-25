cb4367a74d87f12388e8f80199b4e8ded31e066a
Giovane Derenevick <giovanedfilho@gmail.com>
2020-02-24 22:24:30 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/giovanederenevick/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.giovanederenevick.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.giovanederenevick.cursomc.domain.Pedido;
+import com.giovanederenevick.cursomc.repositories.PedidoRepository;
+import com.giovanederenevick.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	public Pedido buscar(Integer id){
+		
+		Optional<Pedido> obj = pedidoRepository.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
