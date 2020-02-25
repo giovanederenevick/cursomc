159e0cc57b3d7a2b9d01c64a824bd40a0b1aa420
Giovane Derenevick <giovanedfilho@gmail.com>
2020-02-25 19:49:43 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/giovanederenevick/cursomc/services/CategoriaService.java
+++ src/main/java/com/giovanederenevick/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.giovanederenevick.cursomc.domain.Categoria;
+import com.giovanederenevick.cursomc.dto.CategoriaDTO;
 import com.giovanederenevick.cursomc.repositories.CategoriaRepository;
 import com.giovanederenevick.cursomc.services.exceptions.DataIntegrityException;
 import com.giovanederenevick.cursomc.services.exceptions.ObjectNotFoundException;
@@ -61,5 +62,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto){
+		
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
